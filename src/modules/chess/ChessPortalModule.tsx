79c8ae5e11bbf08ded3afd543137d636d7399451
maxandbe1 <maxandbe1@proton.me>
2026-05-22 11:38:57 -0400

    Create ChessPortalModule component for Chess Suite

---
+++ src/modules/chess/ChessPortalModule.tsx
@@ -0,0 +1,15 @@
+export default function ChessPortalModule() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Chess Suite</h1>
+      <p>This is the web entry for the Chess Suite.</p>
+
+      <ul>
+        <li>Training</li>
+        <li>Puzzles</li>
+        <li>Analysis</li>
+      </ul>
+    </div>
+  );
+}
+
